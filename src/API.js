1e7b5a7f47b47b1e30d385ddcf2d3ee2e3de9445
richardmaccaw <ricci6@gmail.com>
2019-01-22 10:32:16 +0000

    refactored api code

--- src/API.js
+++ src/API.js
@@ -1,37 +1,45 @@
 class API {
 
-    static findOrCreateUser = (user) => {
-        return fetch('http://localhost:3002/api/v1/users', {
+    static init () {
+        this.baseUrl = 'http://localhost:3002/api/v1'
+        this.usersUrl = this.baseUrl + '/users'
+        this.announcementsUrl = this.baseUrl + '/announcements'
+        this.linksUrl = this.baseUrl + '/links'
+        this.timezonesUrl = this.baseUrl + '/timezones'
+    }
+
+    static findOrCreateUser (user) {
+        return fetch(this.usersUrl, {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify(user)
         }).then(resp => resp.json())
     }
 
-    static postAnnouncement = (announcement) => {
-        return fetch('http://localhost:3002/api/v1/announcements', {
+    static postAnnouncement (announcement) {
+        return fetch(this.announcementsUrl, {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify(announcement)
         }).then(resp => resp.json())
     }
 
-    static deleteAnnouncement = (id) => {
-        return fetch(`http://localhost:3002/api/v1/announcements/${id}`, {
+    static deleteAnnouncement (id) {
+        return fetch(`${this.announcementsUrl}/${id}`, {
             method: 'DELETE'
         }).then(resp => resp.json())
     }
 
-    static patchAnnouncement = (announcement) => {
-        return fetch(`http://localhost:3002/api/v1/announcements/${announcement.id}`, {
+    static patchAnnouncement (announcement) {
+        return fetch(`${this.announcementsUrl}/${announcement.id}`, {
             method: 'PATCH',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify(announcement)
         }).then(resp => resp.json())
     }
 
-    static patchLink = (link) => {
-        return fetch(`http://localhost:3002/api/v1/links/${link.id}`, {
+    static patchLink (link) {
+        return fetch(`${this.linksUrl}/${link.id}`, {
             method: 'PATCH',
             headers: {
                 'Content-Type': 'application/json'
@@ -40,8 +48,8 @@ class API {
         }).then(resp => resp.json())
     }
 
-    static postLink = (link) => {
-        return fetch('http://localhost:3002/api/v1/links', {
+    static postLink (link) {
+        return fetch(this.linksUrl, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -50,14 +58,14 @@ class API {
         }).then(resp => resp.json())
     }
 
-    static deleteLink = (id) => {
-        return fetch(`http://localhost:3002/api/v1/links/${id}`, {
+    static deleteLink (id) {
+        return fetch(`${this.linksUrl}/${id}`, {
             method: 'DELETE'
         }).then(resp => resp.json())
     }
 
-    static postTimezone = (timezone) => {
-        return fetch('http://localhost:3002/api/v1/timezones', {
+    static postTimezone (timezone) {
+        return fetch(this.timezonesUrl, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -66,8 +74,8 @@ class API {
         }).then(resp => resp.json())
     }
 
-    static patchTimezone = (timezone) => {
-        return fetch(`http://localhost:3002/api/v1/timezones/${timezone.id}`, {
+    static patchTimezone (timezone) {
+        return fetch(`${this.timezonesUrl}/${timezone.id}`, {
             method: 'PATCH',
             headers: {
                 'Content-Type': 'application/json'
@@ -76,11 +84,13 @@ class API {
         }).then(resp => resp.json())
     }
 
-    static deleteTimezone = (id) => {
-        return fetch(`http://localhost:3002/api/v1/timezones/${id}`, {
+    static deleteTimezone (id) {
+        return fetch(`${this.timezonesUrl}/${id}`, {
             method: 'DELETE'
         }).then(resp => resp.json())
     }
 }
+
+API.init()
 
 export default API
