19a6379e1e630f9fd8565e3429814e81fa2b1850
richardmaccaw <ricci6@gmail.com>
2019-01-23 14:26:33 +0000

    updated h2 copy

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -35,7 +35,7 @@ const LandingPage = (props) => {
         <div className="container">
             <div className="hero">
                 <Typography className='hero_item hero_h1' variant="h4">A chrome extension to spread company announcements</Typography>
-                <Typography className = 'hero_item hero_h2' variant = "subtitle1"> It gets harder to spread information as companies grow. Knowledge gets lost between departments. CompanyTab helps you spread announcements, important links and more to your employees.</Typography>
+                <Typography className = 'hero_item hero_h2' variant = "subtitle1"> It gets harder to spread information as companies grow. Updates gets lost between departments, drowned out in Slack feeds or clutter inboxes. CompanyTab is a simple tool to help you spread announcements, important links and more to your employees.</Typography>
                 <Button onClick={() => window.open(CHROME_EXTENSION, "_blank")} id="lpButton">Add the Chrome Extension</Button>
                 <img className="hero_item" src={ui} alt='picture of the product ui'/>
             </div>
